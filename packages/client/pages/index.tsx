01d2ed4cf750c3549235f7cb18032a442cbf611f
Farayola Joshua <farayolajoshua@gmail.com>
2021-09-03 15:31:12 +0100

    chore: update monorepo to use lerna alone

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -52,8 +52,6 @@ export default function Home() {
   const [cd] = useMutation(CD);
   const { loading, data, error } = useSubscription(CDALT);
 
-  console.log(loading, data, error);
-
   useEffect(() => {
     const id = setTimeout(() => cd({ variables: { dir: '' } }), 5000);
     return () => clearTimeout(id);
